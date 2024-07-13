be6771dd0ebe84840ab193870138e5f326726436
Luan Peixoto <peixotoluan2023@gmail.com>
2024-07-13 19:48:39 -0300

    Definição da classe Account e classe herdada BusinessAccount

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        BusinessAccount account = new BusinessAccount();
+
+        account.getBalance();
+    }
+}
